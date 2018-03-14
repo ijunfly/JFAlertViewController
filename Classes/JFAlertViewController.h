f4fbf6fe676c7326e760c51dc7ee864b84f59114
张俊飞 <zhangjunfei@zhujia360.com>
2018-03-14 14:46:11 +0800

    更新文件位置

--- Classes/JFAlertViewController.h
+++ Classes/JFAlertViewController.h
@@ -37,20 +37,20 @@
 @interface JFAlertViewController : UIAlertController
 
 /** JFAlertViewDataSource */
-@property (nonatomic, weak) id <JFAlertViewDataSource> dataSource;
+@property (weak, nonatomic) id <JFAlertViewDataSource> dataSource;
 /** JFAlertViewDelegate */
-@property (nonatomic, weak) id <JFAlertViewDelegate> delegate;
+@property (weak, nonatomic) id <JFAlertViewDelegate> delegate;
 
 /** 取消按钮action 若有delegate方法则此block无效 */
 @property (copy, nonatomic) void(^cancelBlock)(JFAlertViewController *alertView);
 /** 点击按钮action 若有delegate方法则此block无效 */
-@property (copy, nonatomic) void(^clickedBlock)(JFAlertViewController *alertView, NSUInteger index);
+@property (copy, nonatomic) void(^actionBlock)(JFAlertViewController *alertView, NSUInteger index);
 
 /** 可以自定义cancel按钮 默认是'取消' */
-@property (strong, nonatomic) NSString *cancelTitle;
+@property (copy, nonatomic) NSString *cancelTitle;
 
-/** 默认除取消按钮外的一个按钮 若有datasource方法则title无效 */
-@property (strong, nonatomic) NSString *defaultFirstTitle;
+/** 默认除取消按钮外的按钮 若有datasource方法则title无效 */
+@property (copy, nonatomic) NSArray <NSString *> *actionTitles;
 
 
 /**
@@ -60,13 +60,17 @@
  @param preferredStyle alert的显示style
  @param cancel cancel按钮的title
  @param cancelBlock cancel的action
- @param defaultText 确认按钮的title
- @param clickBlock 确认的action
+ @param actionTexts 确认按钮的title
+ @param actionBlock 确认的action
  @return JFAlertViewController
  */
-+ (JFAlertViewController *)alertViewWithTitle:(NSString *)title message:(NSString *)message preferredStyle:(UIAlertControllerStyle)preferredStyle
-                                 cancelAction:(NSString *)cancel cancelBlock:(void (^)(JFAlertViewController *alertView))cancelBlock
-                                defaultButton:(NSString *)defaultText defaultBlock:(void (^)(JFAlertViewController *alertView, NSUInteger index))clickBlock;
++ (JFAlertViewController *)alertViewWithTitle:(NSString *)title
+                                      message:(NSString *)message
+                               preferredStyle:(UIAlertControllerStyle)preferredStyle
+                                 cancelButton:(NSString *)cancel
+                                  cancelBlock:(void (^)(JFAlertViewController *alertView))cancelBlock
+                                 actionButton:(NSArray <NSString *> *)actionTexts
+                                  actionBlock:(void (^)(JFAlertViewController *alertView, NSUInteger index))actionBlock;
 
 
 @end
--- Classes/JFAlertViewController.m
+++ Classes/JFAlertViewController.m
@@ -21,12 +21,9 @@
     // Do any additional setup after loading the view.
     
     __weak __typeof__ (self) wSelf = self;
-    
     // 设置 取消按钮
-    NSString *cancelTitle = @"取消";
-    if (self.cancelTitle) {
-        cancelTitle = self.cancelTitle;
-    }
+    NSString *cancelTitle = self.cancelTitle ?: @"取消";
+    
     UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:cancelTitle style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
         __strong __typeof__ (wSelf) sSelf = wSelf;
         if (sSelf.delegate && [sSelf.delegate respondsToSelector:@selector(JFAlertViewCancel:)]) {
@@ -41,27 +38,25 @@
     // 设置其他按钮
     if ((self.dataSource && [self.dataSource respondsToSelector:@selector(numberOfIndexsInJFAlertView:)])) {
         self.items = [self.dataSource numberOfIndexsInJFAlertView:self];
-        self.defaultFirstTitle = nil;
+        self.actionTitles = nil;
         NSAssert(([self.dataSource respondsToSelector:@selector(JFAlertView:titleAtIndexNumber:)]), @"JFAlertViewController // dataSource 实现'numberOfIndexsInJFAlertView:'之后必须实现'JFAlertView:titleAtIndexNumber:'");
         
-    } else if (self.defaultFirstTitle) {
-        self.items = 1;
+    } else if (self.actionTitles) {
+        self.items = self.actionTitles.count;
     } else {
         self.items = 0;
     }
     
-    if (self.items == 0) {
-        return;
-    }
+    if (self.items == 0) return;
 
     for (int i = 0; i < self.items; i++) {
-        NSString *title = (self.items == 1 && self.defaultFirstTitle) ? self.defaultFirstTitle : [self.dataSource JFAlertView:self titleAtIndexNumber:i];
+        NSString *title = (self.actionTitles) ? self.actionTitles[i] : [self.dataSource JFAlertView:self titleAtIndexNumber:i];
         [self addAction:[UIAlertAction actionWithTitle:title style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
             __strong __typeof__ (wSelf) sSelf = wSelf;
             if (sSelf.delegate && [sSelf.delegate respondsToSelector:@selector(JFAlertView:clickedAtIndex:)]) {
                 [sSelf.delegate JFAlertView:sSelf clickedAtIndex:i];
-            } else if (sSelf.clickedBlock) {
-                sSelf.clickedBlock(sSelf, i);
+            } else if (sSelf.actionBlock) {
+                sSelf.actionBlock(sSelf, i);
             }
         }]];
     }
@@ -73,13 +68,20 @@
     return [super alertControllerWithTitle:title message:message preferredStyle:preferredStyle];
 }
 
-+ (JFAlertViewController *)alertViewWithTitle:(NSString *)title message:(NSString *)message preferredStyle:(UIAlertControllerStyle)preferredStyle cancelAction:(NSString *)cancel cancelBlock:(void (^)(JFAlertViewController *alertView))cancelBlock defaultButton:(NSString *)defaultText defaultBlock:(void (^)(JFAlertViewController *alertView, NSUInteger index))clickBlock {
++ (JFAlertViewController *)alertViewWithTitle:(NSString *)title
+                                      message:(NSString *)message
+                               preferredStyle:(UIAlertControllerStyle)preferredStyle
+                                 cancelButton:(NSString *)cancel
+                                  cancelBlock:(void (^)(JFAlertViewController *alertView))cancelBlock
+                                 actionButton:(NSArray <NSString *> *)actionTexts
+                                  actionBlock:(void (^)(JFAlertViewController *alertView, NSUInteger index))actionBlock {
+    
     JFAlertViewController *doneBaseAlertView = [JFAlertViewController alertControllerWithTitle:title message:message preferredStyle:preferredStyle];
     doneBaseAlertView.cancelTitle = cancel;
     doneBaseAlertView.cancelBlock = cancelBlock;
-    if (defaultText) {
-        doneBaseAlertView.defaultFirstTitle = defaultText;
-        doneBaseAlertView.clickedBlock = clickBlock;
+    if (actionTexts) {
+        doneBaseAlertView.actionTitles = actionTexts;
+        doneBaseAlertView.actionBlock = actionBlock;
     }
     return doneBaseAlertView;
 }
